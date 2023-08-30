8dd5edf6bb2a14f925eee0a7c1ac5a624710c246
Hyeonji-Son <sustka46@gmail.com>
2023-08-30 13:52:49 +0900

    [230830]3-2강 까지

--- lib/main.dart
+++ lib/main.dart
@@ -27,14 +27,85 @@ class App extends StatelessWidget {
     // 우선 기본적인 root UI setting은 해줘야함.
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          elevation: 5,
-          title: Text('Hello flutter!'),
-        ),
-        body: Center(
-          heightFactor: 20.0,
-          child: Text('Hello world!'),
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text('Hey, Selena',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text('Welcome back',
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.6),
+                              fontSize: 15,
+                            ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8),
+                  )
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  '\$5 194 482',
+                  style: TextStyle(
+                    fontSize: 42,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  )
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF2B33A),
+                        borderRadius: BorderRadius.circular(45)
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical:20,
+                            horizontal: 50
+                        ),
+                        child: Text('Transfer',
+                          style: TextStyle(
+                            fontSize: 22,
+                          )
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+          ),
         ),
         //class를 찍을 때 마다 ,(콤마)를 찍어주면 vsCode가 자동으로 아래처럼 class가 닫힌 부분을 표시해준다.
       ),
